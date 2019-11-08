9fe93de6341a2f8e8c07906cc491fa3818c2401d
jwalk166 <jwalk166@uncc.edu>
2019-11-08 21:20:55 +0000

    Added authentication

--- Gifto/config/routes.rb
+++ Gifto/config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   end
   # resources :wishlistitems, :path => "MyWishListItems"
   
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
