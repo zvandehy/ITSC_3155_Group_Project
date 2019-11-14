5fee06a58f91d2dfd26c10020ec486992bf853e3
Zeke <ec2-user@ip-172-31-89-63.ec2.internal>
2019-11-14 15:21:21 +0000

    Fix Login forms

--- Gifto/app/controllers/gifts_controller.rb
+++ Gifto/app/controllers/gifts_controller.rb
@@ -1,6 +1,5 @@
 class GiftsController < ApplicationController
     def create
-        print("Zero")
         @user = User.find(params[:user_id])
         @gift = @user.gifts.create(gift_params)
         redirect_to user_path(@user)
@@ -8,7 +7,6 @@ class GiftsController < ApplicationController
 
     private 
         def gift_params
-            print("ONe")
             params.require(:gift).permit(:name, :description)
         end
 end
