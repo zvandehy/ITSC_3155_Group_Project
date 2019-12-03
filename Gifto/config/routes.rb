e19e58767648f62ed41a2b5d9fbb6b63786f3647
J.R <jwalk166@uncc.edu>
2019-12-03 16:07:30 +0000

    Changed homepage

--- Gifto/config/routes.rb
+++ Gifto/config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
-  root 'welcome#index'
+  root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
